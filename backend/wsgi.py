1e3f7f376e46caa18cc634e533cdb57696bdd1c0
cyberdexter82 <alejandrocazango@gmail.com>
2025-11-11 15:34:35 -0600

    FIX: Envolver la app con WhiteNoise en wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+# --- 👇 1. Importa WhiteNoise y settings ---
+from whitenoise import WhiteNoise
+from django.conf import settings
+# ---
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
+# --- 2. Obtiene la aplicación Django estándar ---
 application = get_wsgi_application()
+
+# --- 👇 3. Envuelve la aplicación con WhiteNoise ---
+# WhiteNoise ahora buscará archivos en la carpeta que definiste en STATIC_ROOT
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+# ---
